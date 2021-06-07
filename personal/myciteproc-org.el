61b1775b8aff9ba4c73f69798545794eb5326354
Joseph Vidal-Rosset <joseph.vidal.rosset@gmail.com>
2021-06-07 17:35:25 +0200

    my_prelude_emacs

---
+++ personal/myciteproc-org.el
@@ -0,0 +1,7 @@
+;;; myciteproc-org.el ---                            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Joseph VIdal-Rosset
+
+;; Author: Joseph VIdal-Rosset <joseph@vidal-rosset.net>
+;; Keywords: convenience, abbrev,
+(citeproc-org-setup)
